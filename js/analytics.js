de733f3d7703eacf16fc4c7894eddbe8f424e80b
Zac Heisey <zheisey@gmail.com>
2023-03-07 12:41:10 +0200

    add GA4 config to existing Google tag (gtag.js)

--- js/analytics.js
+++ js/analytics.js
@@ -2,4 +2,8 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
+// Universal Analytics config
 gtag('config', 'UA-10285388-1');
+
+// GA4 config
+gtag('config', 'G-1ZDWTEE6J9');
